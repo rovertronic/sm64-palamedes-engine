7b4f9d8c6adff78145cd8c36ec93eea01bbdc221
RRovert <rovertentertainment@gmail.com>
2024-03-31 01:49:30 -0500

    turn off debug

--- include/config/config_debug.h
+++ include/config/config_debug.h
@@ -18,7 +18,7 @@
  * Enables a comprehensive standalone profiler. Automatically enabled by PUPPYPRINT_DEBUG.
  * If not using PUPPYPRINT_DEBUG, press L + D-Pad Up to toggle the profiler.
  */
-#define USE_PROFILER
+//#define USE_PROFILER
 
 /**
  * -- TEST LEVEL --
@@ -31,12 +31,12 @@
 /**
  * Enables debug level select. Hold L while the game boots to turn it on.
  */
-#define DEBUG_LEVEL_SELECT
+//#define DEBUG_LEVEL_SELECT
 
 /**
  * Enables debug free move (D-pad up to enter, A to exit).
  */
-#define ENABLE_DEBUG_FREE_MOVE
+//#define ENABLE_DEBUG_FREE_MOVE
 
 /**
  * Enables a custom, enhanced performance profiler. (Enables PUPPYPRINT by default in config_safeguards).
@@ -58,7 +58,7 @@
  * Visual debug enables some collision visuals. Tapping Right on the D-pad will cycle between visual hitboxes, visual surfaces, both, and neither.
  * If puppyprint is enabled, then this can be cycled only while the screen is active.
  */
-#define VISUAL_DEBUG
+//#define VISUAL_DEBUG
 
 /**
  * Opens all courses and doors. Used for debugging purposes to unlock all content.
